94f4746ca2d43a543f480f2ffdc85e18977aa53e
Your Name <you@example.com>
2020-01-29 21:38:40 +0200

    first commit-test

---
+++ application/modules/admin/views/dashboard.php
@@ -0,0 +1,18 @@
+<!-- Info boxes -->
+
+
+<!-- /.row -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="col-md-12 col-sm-12">
+                    <h1 style="color: #7d8997;font-weight: bold;text-transform:uppercase;display:inline-block;">
+                       <img src="<?php echo base_url('assets/images/credit/'.$_SESSION['settings_data']['settings_file_path']); ?>" width="80px" />
+                        <?php echo $_SESSION['settings_data']['settings_name']; ?>
+                     </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ assets/grocery_crud/languages/hebrew.php
@@ -0,0 +1,93 @@
+<?php
+	$lang['list_add'] 				= 'הוסף';
+	$lang['list_actions'] 			= 'פעולות';
+	$lang['list_page'] 				= 'עמוד';
+	$lang['list_paging_of'] 		= 'מ';
+	$lang['list_displaying']		= 'מציג {start} עד {end} מ {results} רשומות';
+	$lang['list_filtered_from']		= '(מסונן מ {total_results} רשומות)';
+	$lang['list_show_entries']		= 'הצג {paging} רשומות';
+	$lang['list_no_items']			= 'אין רשומות להצגה';
+	$lang['list_zero_entries']		= 'מציג 0 עד 0 מ 0 רשומות';
+	$lang['list_search'] 			= 'חפש';
+	$lang['list_search_all'] 		= 'חפש הכל';
+	$lang['list_clear_filtering'] 	= 'נקה פילטר';
+	$lang['list_delete'] 			= 'מחק';
+	$lang['list_edit'] 				= 'ערוך';
+	$lang['list_paging_first'] 		= 'ראשון';
+	$lang['list_paging_previous'] 	= 'קודם';
+	$lang['list_paging_next'] 		= 'הבא';
+	$lang['list_paging_last'] 		= 'אחרון';
+	$lang['list_loading'] 			= 'טוען נתונים...';
+
+	$lang['form_edit'] 				= 'ערוך';
+	$lang['form_back_to_list'] 		= 'בחזרה לרשימה';
+	$lang['form_update_changes'] 	= 'עדכן שינויים';
+	$lang['form_cancel'] 			= 'בטל';
+	$lang['form_update_loading'] 	= 'המתן, מעדכן שינויים...';
+	$lang['update_success_message'] = 'השינויים נשמרו בהצלחה.';
+	$lang['form_go_back_to_list'] 	= 'בחזרה לרשימה';
+
+	$lang['form_add'] 				= 'הוסף';
+	$lang['insert_success_message'] = 'הנתונים הוספו בהצלחה לבסיס הנתונים.';
+	$lang['form_or']				= 'או';
+	$lang['form_save'] 				= 'שמור';
+	$lang['form_insert_loading'] 	= 'המתן, שומר נתונים...';
+
+	$lang['form_upload_a_file'] 	= 'העלה קובץ';
+	$lang['form_upload_delete'] 	= 'מחק';
+	$lang['form_button_clear'] 		= 'נקה';
+
+	$lang['delete_success_message'] = 'הנתונים נמחקו בהצלחה מבסיס הנתונים.';
+	$lang['delete_error_message'] 	= 'הנתונים לא נמחקו מבסיס הנתונים.';
+
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'המידע שהזנת אינו נשמר.\\nהאם ברצונך לחזור לרשימה?';
+	$lang['alert_edit_form']		= 'הנתונים ששינית לא נשמרו.\\nהאם ברצונך לחזור לרשימה?';
+	$lang['alert_delete']			= 'האם אתה בטוח שברצונך למחוק רשומה זו?';
+
+	$lang['insert_error']			= 'הייתה בעיה בהכנסת הנתונים.';
+	$lang['update_error']			= 'הייתה בעיה בעדכון הנתונים.';
+
+	/* Added in version 1.2.1 */
+	$lang['set_relation_title']		= 'בחר {field_display_as}';
+	$lang['list_record']			= 'רשומה';
+	$lang['form_inactive']			= 'לא';
+	$lang['form_active']			= 'כן';
+
+	/* Added in version 1.2.2 */
+	$lang['form_save_and_go_back']	= 'שמור וחזור לרשימה';
+	$lang['form_update_and_go_back']= 'עדכן וחזור לרשימה';
+
+	/* Upload functionality */
+	$lang['string_delete_file'] 	= "מוחק קובץ";
+	$lang['string_progress'] 		= "קצב התקדמות: ";
+	$lang['error_on_uploading'] 	= "היתה בעיה בזמן העלאת הקובץ.";
+	$lang['message_prompt_delete_file'] 	= "האם אתה בטוח שברצונך למחוק קובץ זה?";
+
+	$lang['error_max_number_of_files'] 	= "ניתן להעלות קובץ אחד בלבד בכל פעם.";
+	$lang['error_accept_file_types'] 	= "אינך רשאי להעלות קובץ עם סיומת זו.";
+	$lang['error_max_file_size'] 		= "הקובץ שהועלה חרג מהגודל המקסימלי של {max_file_size}.";
+	$lang['error_min_file_size'] 		= "אי אפשר להעלות קובץ ריק.";
+
+	/* Added in version 1.3.1 */
+	$lang['list_export'] 	= "ייצא";
+	$lang['list_print'] 	= "הדפס";
+	$lang['minimize_maximize'] = 'הראה/הסתר';
+
+	/* Added in version 1.4 */
+	$lang['list_view'] = 'פרטי';
+
+		/* Added in version 1.5.1 */
+		$lang['ui_day'] = 'dd';
+		$lang['ui_month'] = 'mm';
+		$lang['ui_year'] = 'yyyy';
+
+		/* Added in version 1.5.2 */
+		$lang['list_more'] = 'More';
+
+		/* Added in version 1.5.6 */
+		$lang['list_search_column'] = 'Search {column_name}';
+
+		/* Added in version 1.5.8 */
+		$lang['alert_delete_multiple'] = 'Are you sure that you want to delete those {items_amount} items?';
+		$lang['alert_delete_multiple_one'] = 'Are you sure that you want to delete this 1 item?';
